7614f1773921b72cf7f4e84fc85cc28fcc57b535
muhadzdzib al khairy <113493493+muhadzdzib-al-khairy@users.noreply.github.com>
2023-01-24 18:55:27 +0700

    Add files via upload

---
+++ buku-service/src/main/java/com/khairy/buku/entity/Buku.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.khairy.buku.entity;
+
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author 
+ */
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Buku {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long bukuId;
+    private String kode;
+    private String judul;
+    private String pengarang;
+    private String penerbit;
+    private String tahunTerbit;
+}
