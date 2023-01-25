90ae0882cc0d58530c3e3e5aa30fe3ba3da640c3
muhadzdzib al khairy <113493493+muhadzdzib-al-khairy@users.noreply.github.com>
2023-01-25 23:05:36 +0700

    Add files via upload

---
+++ client_2101082010/src/main/java/com/khairy/client_2101082010/model/Pengembalian.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.khairy.client_2101082010.model;
+
+/**
+ *
+ * @author Khairy
+ */
+public class Pengembalian {
+    private Long pengembalianId;
+    private Long peminjamanId;
+    private String tglDikembalikan;
+    private int terlambat;
+    private double denda;
+
+    public Pengembalian() {
+    }
+    
+    
+    public Pengembalian(Long pengembalianId, Long peminjamanId, String tglDikembalikan, int terlambat, double denda) {
+        this.pengembalianId = pengembalianId;
+        this.peminjamanId = peminjamanId;
+        this.tglDikembalikan = tglDikembalikan;
+        this.terlambat = terlambat;
+        this.denda = denda;
+    }
+    
+    
+    
+    public Long getPengembalianId() {
+        return pengembalianId;
+    }
+
+    public void setPengembalianId(Long pengembalianId) {
+        this.pengembalianId = pengembalianId;
+    }
+
+    public Long getPeminjamanId() {
+        return peminjamanId;
+    }
+
+    public void setPeminjamanId(Long peminjamanId) {
+        this.peminjamanId = peminjamanId;
+    }
+
+    public String getTglDikembalikan() {
+        return tglDikembalikan;
+    }
+
+    public void setTglDikembalikan(String tglDikembalikan) {
+        this.tglDikembalikan = tglDikembalikan;
+    }
+
+    public int getTerlambat() {
+        return terlambat;
+    }
+
+    public void setTerlambat(int terlambat) {
+        this.terlambat = terlambat;
+    }
+
+    public double getDenda() {
+        return denda;
+    }
+
+    public void setDenda(double denda) {
+        this.denda = denda;
+    }
+
+    
+    
+    
+}
